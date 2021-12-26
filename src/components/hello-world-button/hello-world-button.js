5a14b80ccfdef35dc789b2730a97875670d55ef0
DarkJacket <ftcp.97@gmail.com>
2021-12-26 19:34:50 -0300

    first

---
+++ src/components/hello-world-button/hello-world-button.js
@@ -0,0 +1,22 @@
+import './hello-world-button.scss'
+
+export default class HelloWorldButton {
+
+    buttonCssClass = 'hello-world-button'
+
+    Render(){
+        const button = document.createElement('button');
+        button.innerHTML = 'Hello World';
+
+        button.classList.add(this.buttonCssClass);
+
+        document.body.appendChild(button);
+
+        button.onclick = () => {
+            const p = document.createElement('p');
+            p.innerText = 'hello world';
+            p.classList.add('hello-world-text')
+            document.body.appendChild(p);
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+
+
+
+const helloWorld = new HelloWorldButton();
+helloWorld.Render();
